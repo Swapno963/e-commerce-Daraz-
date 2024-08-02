fd499b24e9c9d407fc8680fc05ea70d4ff5c50d7
swapno963 <swapno963@gmail.com>
2024-08-02 23:08:09 +0600

    Basic design added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Topbanner from "./components/Topbanner";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Topbanner />
+      <Footer />
+    </div>
+  );
+}
